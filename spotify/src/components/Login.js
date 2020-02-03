ea6dfbfe10d0377259ebbf940a61c51388052140
raj u <r@Rs-MacBook-Air.local>
2020-02-03 11:02:15 -0800

    initial login and register

---
+++ spotify/src/components/Login.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState} from "react";
+import axios from 'axios';
+
+export default function Login() {
+    const [state, setState] = useState ({userName: "", password: ""})
+    return  (
+        <form>
+
+
+        </form>
+    );
+
+} 
---
+++ spotify/src/components/Register.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useState} from "react";
+import axios from 'axios';
+
+export default function SignUp() {
+    const [addTeamMembers, setAddTeamMembers] = React.useState([{
+
+      },
+
+    ])
+
+
+
+
+    return (
+      <div className="App">
+        <h1>Sign Up Below!</h1>
+
+        <NewAddTeamMemberForm 
+
+          addTeamMembers={addTeamMembers} 
+          setAddTeamMembers={setAddTeamMembers}
+
+        />
+
+        <AddTeamMembers 
+          addTeamMembers={addTeamMembers} 
+
+         />
+      </div>
+    );
+  }
+
+  function NewAddTeamMemberForm(props) {
+    const [newTeamMember, setNewTeamMember] = React.useState({
+      name: "",
+      email: "",
+      password: "",
+      
+    })
+
+    const handleSubmit = (event) => {
+      event.preventDefault()
+      axios.post("", newTeamMember)
+          .then(response =>{
+              console.log(response)
+
+          })
+          .catch(err => {
+              console.log(err)
+          })
+
+
+    }
+
+    const handleChange = (event) => {
+      setNewTeamMember({...newTeamMember, [event.target.name]: event.target.value })
+    }
+
+    return (
+
+      <form onSubmit={handleSubmit}>
+
+      <div className="inputContainer">
+        <input 
+          type="text"
+          name="name"
+          placeholder="enter your first and last names"
+          onChange={handleChange}
+          value={newTeamMember.name}
+        />
+        </div>
+
+      
+
+        <div className="inputContainer">
+          <input 
+            type="text"
+            name="email"
+            placeholder="someone@example.com"
+            onChange={handleChange}
+            value={newTeamMember.email}
+          />
+
+        </div>
+
+        <div className="inputContainer">
+          <input 
+            type="password"
+            name="password"
+            placeholder="enter your password"
+            onChange={handleChange}
+            value={newTeamMember.password}
+          />
+        </div>
+
+        
+
+      {props.isEditing ? <button role="submit">Edit team member</button> : <button role="submit">Sign Up!</button>}
+    </form>
+    )
+  }
+
+  function TeamMember({member, handleEdit, handleDelete}) { return (
+    <div className="team-member">
+
+    </div>
+  )}
+
+  function AddTeamMembers(props) {
+    const renderTeamMember = function mapperFn(member) {
+      return <TeamMember 
+                member={member}
+                handleEdit={props.handleEdit} 
+                handleDelete={props.handleDelete}
+              />
+    }
+
+    return (
+      <div className="team-members">
+        {props.addTeamMembers.map(renderTeamMember)}
+      </div>
+    )
+
+}
