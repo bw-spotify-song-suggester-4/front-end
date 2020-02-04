3292af508de447278d00ce7eac7cc1fecb13bb59
ajmerr13 <oliverclosoff07@gmail.com>
2020-02-04 12:36:35 -0500

    favorite reducer added and actions index updated

--- spotify/src/components/Dashboard.js
+++ spotify/src/components/Dashboard.js
@@ -4,7 +4,7 @@ import React from 'react';
 export const Dashboard = () => {
  return (
   <div>
-
+   <h1>This is the Dashboard</h1>
   </div>
    );
 }
--- spotify/src/reducers/index.js
+++ spotify/src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from "redux";
 // Import all reducer files here
 import { loginReducer } from "./loginReducer";
-
+import { favoriteReducer } from "./favoriteReducer"
 
 
 const allReducers = combineReducers({
   loginState: loginReducer
+  
 });
 
 export default allReducers;
