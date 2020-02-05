2ae588bdc2c61b98859e9cccaa27047fe4cb1e30
ajmerr13 <oliverclosoff07@gmail.com>
2020-02-05 11:25:07 -0500

    privateroute updated

--- spotify/src/components/Dashboard.js
+++ spotify/src/components/Dashboard.js
@@ -1,10 +1,17 @@
 import React from "react";
 import {connect} from "react-redux";
 
-export const Dashboard = props => {
+export const Dashboard = (props) => {
+   const id = props.match.params.id;
+  
+  console.log(props, id)
+
+
   return (
+    
     <div>
       <h1>This is the Dashboard</h1>
+      
     </div>
   );
 };
