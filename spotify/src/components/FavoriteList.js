21d3850bbef8d600ab2a62edef3d6b3b851d90ee
raj u <r@Rs-MacBook-Air.local>
2020-02-06 15:48:08 -0800

    latest update

--- spotify/src/components/FavoriteList.js
+++ spotify/src/components/FavoriteList.js
@@ -3,35 +3,5 @@ import axiosWithAuth from '../utils/axiosWithAuth'
 import FavoriteCard from './FavoriteCard'
 import {songs} from "./DummyData";
 
-function FavoriteList(){
-    const [data, setData] = useState([]);
-    console.log(songs)
-        useEffect(() =>{
-            axiosWithAuth()
-            .get("accounts/favorites")
-            .then(response =>{
-                setData(response.data);
-                console.log(response)
-            })
-        },[])
-        return(
-            <div>
-             
-            
-                {songs.map((data)=>{
-                    return(
-                        <div id={data.track_id}> 
-                        <FavoriteCard  
-                            id={data.track_id}
-                            artist = {data.artist_name}
-                            title = {data.track_name}
-                            cover_url= {data.cover_url}
-                        />
-                        </div>
-                    )}
-                )}
-             
-            </div> 
-        );
-    };
+
     export default FavoriteList;
